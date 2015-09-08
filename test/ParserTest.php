67b5f2ea2ea144e2e1fe50d3089ef5307aa1a1ee
mimbert <mimbert@nexweb.ca>
2015-09-08 12:34:46 -0400

    Add a few unit tests

--- test/ParserTest.php
+++ test/ParserTest.php
@@ -1,14 +1,36 @@
 <?php
+
 class ParserTest extends PHPUnit_Framework_TestCase
 {
-    // ...
 
     public function testLoadFile()
     {
-        // Arrange
         $a = new \MathieuImbert\SimpleEmailParser\Parser();
         $a->loadFile(__DIR__ . '/multipart_email.eml');
 
         $this->assertStringEqualsFile(__DIR__ . '/multipart_email.eml', $a->rawData);
+    }
+
+    public function testLoadContent()
+    {
+        $a = new \MathieuImbert\SimpleEmailParser\Parser();
+
+        $content = file_get_contents(__DIR__ . '/multipart_email.eml');
+        $a->loadContent($content);
+
+        $this->assertStringEqualsFile(__DIR__ . '/multipart_email.eml', $a->rawData);
+    }
+
+    public function testParse()
+    {
+        $a = new \MathieuImbert\SimpleEmailParser\Parser();
+        $a->loadFile(__DIR__ . '/multipart_email.eml');
+
+        $a->parse();
+
+        $headers = $a->getHeaders();
+
+        $this->assertCount(22, $headers);
+        $this->assertEquals($a->getHeader('from'), 'Mathieu Imbert <mimbert@nexweb.ca>');
     }
 }
